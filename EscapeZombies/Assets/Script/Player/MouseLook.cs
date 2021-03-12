8ed630933c7b5f5347a96270a016b6888a8a8b98
LAPTOP-1RJR9LME\Acer <jeff-ben10@hotmail.com>
2021-03-12 23:51:12 +0800

    Escape Zombie

--- EscapeZombies/Assets/Script/Player/MouseLook.cs
+++ EscapeZombies/Assets/Script/Player/MouseLook.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
 
-    public float mouseSensitivity = 100f;
+    public float mouseSensitivity = 0.1f;
 
     public Transform playerBody;
 
@@ -20,8 +20,8 @@ public class MouseLook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
