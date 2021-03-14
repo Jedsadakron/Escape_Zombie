0ef0f8075cb88278bf2c77b4cae83de206dd073f
LAPTOP-1RJR9LME\Acer <jeff-ben10@hotmail.com>
2021-03-14 19:09:52 +0800

    Escape Zombie

---
+++ EscapeZombies/Assets/Script/Waypoints/Edge.cs
@@ -0,0 +1,12 @@
+﻿public class Edge
+{
+    // Start and end nodes
+    public Node startNode;
+    public Node endNode;
+
+    public Edge(Node from, Node to)
+    {
+        startNode = from;
+        endNode = to;
+    }
+}
--- EscapeZombies/Assets/Script/Waypoints/Waypoints.cs
+++ EscapeZombies/Assets/Script/Waypoints/Waypoints.cs
@@ -2,17 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public struct Link
+{
+    public GameObject node1;
+    public GameObject node2;
+    public direction dir;
+    public enum direction
+    {
+        TwoWay,
+        OneWay
+    };
+}
+
 public class Waypoints : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject[] waypoints;
+    public Link[] links;
+    public Graph graph = new Graph();
+
+    private void Start()
     {
-        
+        if (waypoints.Length > 0)
+        {
+            foreach (GameObject wp in waypoints)
+            {
+                graph.AddNode(wp);
+            }
+
+            foreach (Link l in links)
+            {
+                graph.AddEdge(l.node1, l.node2);
+                if (l.dir == Link.direction.TwoWay)
+                {
+                    graph.AddEdge(l.node2, l.node1);
+                }
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        graph.debugDraw();
     }
 }
